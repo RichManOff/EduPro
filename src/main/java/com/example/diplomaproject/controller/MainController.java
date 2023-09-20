8ee4bdaef5a8fd7483f9135df7909ed51a7388ec
RichManOff <b.ospan.2002@gmail.com>
2023-09-20 16:41:15 +0600

    Added comment

--- src/main/java/com/example/diplomaproject/controller/MainController.java
+++ src/main/java/com/example/diplomaproject/controller/MainController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
-
+////
 @Slf4j
 @RestController
 @RequestMapping("/home")
